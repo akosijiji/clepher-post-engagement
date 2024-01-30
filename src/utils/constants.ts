cacaab37327185bb158c96680fcbc938f553bf26
Mary Sabio <mlssabio21@gmail.com>
2024-01-31 04:05:27 +0800

    Create constants file

---
+++ src/utils/constants.ts
@@ -0,0 +1,6 @@
+export const EDIT = 'Edit';
+export const RENAME = 'Rename';
+export const DELETE = 'Delete';
+export const DROPDOWN_ACTIONS = [EDIT, RENAME, DELETE];
+export const PAGINATION_DROPDOWN_ACTIONS = [10, 25, 50, 100];
+export const POST_ENGAGEMENT_TH_ITEMS = ['Name', 'Platform', 'Total Engaged/Unique', 'Acquired Subscribers', 'Conversion Rate', 'Actions'];
