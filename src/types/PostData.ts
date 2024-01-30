10cafb83c5b6b187595284af40ef3199ab46a30e
Mary Sabio <mlssabio21@gmail.com>
2024-01-31 04:05:03 +0800

    Create PostData interface

---
+++ src/types/PostData.ts
@@ -0,0 +1,8 @@
+export default interface PostData {
+    id?: any | null,
+    name: string,
+    platform: string,
+    total_engaged: string,
+    acquired_subscribers: string,
+    conversion_rate: string,
+}
